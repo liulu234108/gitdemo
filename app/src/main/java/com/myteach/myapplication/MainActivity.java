c8e80ea270dd28f65061ea77b1d3a09789613553
liulu234108 <3199386319@qq.com>
2020-12-10 17:31:17 +0800

    修改进行提交

--- app/src/main/java/com/myteach/myapplication/MainActivity.java
+++ app/src/main/java/com/myteach/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String s="这是一个基础的git提交";
         String a="更新项目";
+        String c="";
     }
 }
